1b95b41fe73486329850bbcc9c0dbf27ae11e85b
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-09-18 18:57:19 +0300

    specification refactoring

--- spec/features/create_achievement_spec.rb
+++ spec/features/create_achievement_spec.rb
@@ -1,24 +1,17 @@
 require "rails_helper"
+require_relative "../support/new_achievement_form"
 
 feature "createn new achievement" do
-  scenario "create new achievement with valida data" do
-    visit("/")
-    click_on("New Achievement")
-    fill_in("Title", with: "Read a book")
-    fill_in("Description", with: "Excellent read")
-    select("Public", from: "Privacy")
-    check("Featured Achievement")
-    attach_file("Cover image", "#{Rails.root}/spec/fixtures/cover_image.png")
-    click_on("Create Achievement")
+  let(:new_achievement_form) { NewAchievementForm.new }
 
-    expect(page).to have_content("Achievement has been created")
+  scenario "create new achievement with valida data" do
+    new_achievement_form.visit_page.fill_in_with(title: "Read a book").submit
+    expect(page).to have_content("Achievement has been create")
     expect(Achievement.last.title).to eq("Read a book")
   end
-  scenario "can not create achievement with invalid data" do
-    visit("/")
-    click_on("New Achievement")
-    click_on("Create Achievement")
 
+  scenario "can not create achievement with invalid data" do
+    new_achievement_form.visit_page.submit
     expect(page).to have_content('can\'t be blank')
   end
 end
