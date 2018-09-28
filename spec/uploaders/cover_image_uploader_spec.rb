cc6fbf376b812854a210ea6a32e5e89199786d06
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-09-28 20:27:51 +0300

    allow only image uploading

---
+++ spec/uploaders/cover_image_uploader_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe CoverImageUploader do
+  it 'allows only images' do
+    uploader = CoverImageUploader.new(Achievement.new, :cover_image)
+    expect {
+      File.open("#{Rails.root}/spec/fixtures/empty.pdf") do |f|
+        uploader.store!(f)
+      end
+    }.to raise_exception(CarrierWave::IntegrityError)
+  end
+end
